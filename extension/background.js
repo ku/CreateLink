39b8322de4ed5bc88a7d1f9d8eb4e08ac1f599a9
Kentaro ku KUMAGAI <ku0522a@gmail.com>
2017-03-13 00:11:58 +0900

    evaluate filter as code

--- extension/background.js
+++ extension/background.js
@@ -124,9 +124,9 @@ function showPrompt(text, pos, subject) {
 }
 
 function sendMessageToTab(tabId, message) {
-  return _.Deferred(function (d) {
+  return new Promise(function (resolve) {
     chrome.tabs.sendMessage(tabId, message, function (res) {
-      d.resolve(res);
+      resolve(res);
     });
   });
 }
@@ -162,10 +162,10 @@ CreateLink.prototype.formatLinkText = function (formatId, url, text, title, tabI
 
   var m = data.match(/%input%/g);
   if (m) {
-    var inputDeferreds = m.map(function () {
-      return sendMessageToTab(tabId, 'showInputDialog');
+    var inputPromises = m.map(function () {
+      return sendMessageToTab(tabId, {type: 'showInputDialog'});
     });
-    d = _.when.apply(_, inputDeferreds).pipe(function () {
+    d = Promise.all(inputPromises).then(function () {
       var inputs = _.toArray(arguments);
       var index = 0;
       data = data.replace(/%input%/g, function (s) {
@@ -174,15 +174,17 @@ CreateLink.prototype.formatLinkText = function (formatId, url, text, title, tabI
       return data;
     });
   } else {
-    d = _.Deferred().resolve(data);
+    d = Promise.resolve(data);
   }
 
-  d = d.pipe(function (data) {
+  d = d.then(function (data) {
     if (def.filter) {
       var m = def.filter.match(/^s\/(.+?)\/(.*?)\/(\w*)$/);
       if (m) {
         var r = new RegExp(m[1], m[3]);
         data = data.replace(r, m[2]);
+      } else {
+        return sendMessageToTab(tabId, {type: 'evaluateFilter', code: def.filter, string: data})
       }
     }
     return data;
@@ -208,7 +210,7 @@ function onMenuItemClick(formatId, info, tab) {
   var text = info.selectionText || tab.title;
   var title = tab.title;
 
-  instance().formatLinkText(formatId, url, text, title, tab.id).pipe(function (linkText) {
+  instance().formatLinkText(formatId, url, text, title, tab.id).then(function (linkText) {
     instance().copyToClipboard(linkText);
   });
 }
--- extension/content.js
+++ extension/content.js
@@ -1,6 +1,9 @@
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request === 'showInputDialog') {
+  if (request.type === 'showInputDialog') {
     var text = window.prompt("CreateLink needs your input");
     return sendResponse(text);
+  } else if (request.type === 'evaluateFilter') {
+    var f = new Function('s', request.code)
+    return sendResponse(f.call(null, request.string))
   }
 });
